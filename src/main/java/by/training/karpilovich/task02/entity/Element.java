2f11dbcd31c030f5cf78451d05b80e9c4eff456a
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-02-12 11:56:58 +0300

    Element class became final and refactored its toString method

--- src/main/java/by/training/karpilovich/task02/entity/Element.java
+++ src/main/java/by/training/karpilovich/task02/entity/Element.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Element {
+public final class Element {
 	private static final Logger LOGGER = LogManager.getLogger(Element.class);
 
 	private int value;
@@ -55,6 +55,6 @@ public class Element {
 
 	@Override
 	public String toString() {
-		return getClass().getSimpleName() + " [value=" + value + ", lock=" + lock + "]";
+		return getClass().getSimpleName() + " [value=" + value + "]";
 	}
 }
